4221dacb570e7a58976feed6f270f9dd238589d5
Andrew Jane <andrew@8i.com>
2021-12-02 15:29:14 +1300

    Added in part 2 of Day 1, new function, modded error handling

--- Day1/day1.c
+++ Day1/day1.c
@@ -1,22 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define PERROR(x) \
-        fprintf(stderr, "%s\n", x); \
-        return -1;
+#define P_ERROR(x) \
+            perror(x); \
+            return -1;
 
+#define WINDOW_SIZE 3
+
+
+int window(FILE **f)
+{
+    if (f == NULL || *f == NULL)
+    {
+        P_ERROR("File pointer or final pointer address is null");
+    }
+
+    ssize_t curr_fpos = ftell(*f);
+    int prev_depth = 0;
+    size_t n = 0; // Used for getline() only
+    ssize_t index, count = 0;
+    char *line = NULL;
+
+    if ((count = getline(&line, &n, *f)) < 0)
+    {
+        // Can't have gotten to end of file yet, so only errors at this stage
+        P_ERROR("init Failed to read a new line from input file");
+    }
+
+    prev_depth = atoi(line);
+    index = count + curr_fpos;
+
+    for (int i = 0; i < WINDOW_SIZE - 1; ++i)
+    {
+        if ((count = getline(&line, &n, *f)) < 0)
+        {
+            if (feof(*f))
+            {
+                /*
+                 * Chance that given odd multiple of the window size in the file
+                 * we don't want to try read a line that isn't there.
+                 */
+                return prev_depth;
+            }
+            else if (ferror(*f))
+            {
+                P_ERROR("for Failed to read a new line from input file");
+            }
+        }
+
+        prev_depth += atoi(line);
+    }
+
+    fseek(*f, index, SEEK_SET);
+
+    return prev_depth;
+}
 
 int main(int argc, char *argv[])
 {
     if (argc <= 1)
     {
-        PERROR("missing args")
+        fprintf(stderr, "missing args");
+        return -1;
     }
 
     FILE *fp = fopen(argv[1], "r");
     if (fp == NULL)
     {
-        PERROR("Couldn't open input file")
+        fprintf(stderr, "Couldn't open input file");
+        return -1;
     }
 
     if (feof(fp))
@@ -25,19 +77,26 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    int count, prev_depth = 0;
-    size_t len = 0;
-    char *line = NULL;
-    if (getline(&line, &len, fp) < 0) // Get initial value
+    int prev_depth = window(&fp); // Initial value
+    if (prev_depth < 0)
     {
-        PERROR("Failed to read a new line from input file")
+        return -1;
     }
 
-    prev_depth = atoi(line);
-
-    while(getline(&line, &len, fp) >= 0)
+    int count = 0;
+    while (!feof(fp))
     {
-        int depth = atoi(line);
+        if (ferror(fp))
+        {
+            P_ERROR(NULL);
+        }
+
+        int depth = window(&fp);
+        if (prev_depth < 0)
+        {
+            return -1;
+        }
+
         if (depth > prev_depth) // Depth has increased
         {
             count++;
@@ -48,5 +107,7 @@ int main(int argc, char *argv[])
 
     printf("Depth changed: %d times.\n", count);
 
+    fclose(fp);
     return 0;
 }
+
